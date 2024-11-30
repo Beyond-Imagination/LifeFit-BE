39d466bc679179bee12332ebc58d9b10a7e393a9
konempty <jameswebb@naver.com>
2024-11-30 21:51:05 +0900

    좋아요 누른 프로젝트 조회 API 추가

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ const {
   updateUserById,
   deleteUserById,
 } = require("../service/userService");
+const { Project } = require("../models/projects");
 
 const addUser = async (req, res) => {
   try {
@@ -34,11 +35,11 @@ const updateUser = async (req, res) => {
     if (req.user.id !== req.params.id) {
       return res.status(403).json({ error: "Unauthorized" });
     }
-    const { currentPassword, ...updateData } = req.body;
+    const { currentPassword, updateData } = req.body;
     const user = await updateUserById(
       req.params.id,
       currentPassword,
-      updateData
+      updateData,
     );
     if (!user) {
       return res.status(404).json({ error: "User not found" });
@@ -64,9 +65,36 @@ const deleteUser = async (req, res) => {
   }
 };
 
+const getLikedProjects = async (req, res) => {
+  const { userId } = req.user.id;
+
+  try {
+    // 좋아요 배열에 userId가 포함된 프로젝트 검색
+    const likedProjects = await Project.find({ likes: userId });
+
+    res.status(200).json(
+      likedProjects.map((project) => {
+        return {
+          id: project._id,
+          title: project.title,
+          likes: project.likes.length,
+          createdAt: project.createdAt,
+        };
+      }),
+    );
+  } catch (error) {
+    console.error("Error fetching liked projects:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching liked projects",
+    });
+  }
+};
+
 module.exports = {
   addUser,
   getUser,
   updateUser,
   deleteUser,
+  getLikedProjects,
 };
--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -3,13 +3,13 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
   if (!bearerHeader) {
-    return res.status(403).json({ error: "Token is required" });
+    return res.status(401).json({ error: "Token is required" });
   }
   const bearer = bearerHeader.split(" ");
   const token = bearer[1];
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(403).json({ error: "Token is invalid" });
+      return res.status(401).json({ error: "Token is invalid" });
     }
     req.user = decoded;
     next();
